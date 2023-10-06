802a16d2725bed02657657a15f094e0407d5f4e7
Jason Paul Darivemula <jasonpauldj@gmail.com>
2023-10-06 09:42:46 -0400

    Minor changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,17 +3,17 @@ import Link from "next/link"
 export const Navbar = () => {
     return (
         <nav className="bg-primary text-white">
-            <div className="flex justify-between w-screen">
+            <div className="flex justify-between w-screen items-center">
                 <div className="flex-1"></div>
-                <div className="flex-1"><h3>Product Viewer</h3></div>
-            <ul className="flex">
-                <li className="mr-6">
-                    <Link className="hover:text-blue-800" href="/">View Products</Link>
-                </li>
-                <li className="mr-6">
-                    <Link className="hover:text-blue-800" href="/products/search">Search By Category</Link>
-                </li>
-            </ul>
+                <div className="flex-1 text-4xl" ><h3>Product Viewer</h3></div>
+                <ul className="flex">
+                    <li className="mr-6">
+                        <Link className="hover:text-blue-800" href="/">View Products</Link>
+                    </li>
+                    <li className="mr-6">
+                        <Link className="hover:text-blue-800" href="/products/search">Search By Category</Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     )
